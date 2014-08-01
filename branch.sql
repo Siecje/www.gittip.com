512020590309248b4f28c1108fe6063b3410af3d
Changaco <changaco@changaco.oy.lc>
2014-08-01 14:50:08 +0200

    zero out known anomalous tip

--- branch.sql
+++ branch.sql
@@ -6,4 +6,7 @@ BEGIN;
     CREATE TYPE exchange_status AS ENUM ('pre', 'pending', 'failed', 'succeeded');
     ALTER TABLE exchanges ADD COLUMN status exchange_status;
 
+    INSERT INTO tips (ctime, tipper, tippee, amount)
+        SELECT ctime, tipper, tippee, 0 FROM tips WHERE id = 46266;
+
 END;
